c45a379fca2b756327b0b746c4b4fb49ad6fd49f
Jitendra Kumar Yadav <jiteyad@gmail.com>
2020-07-17 08:55:30 +0200

    update

--- src/grundlagen/Rectangle.java
+++ src/grundlagen/Rectangle.java
@@ -4,11 +4,11 @@ import java.awt.*;
 
 public class Rectangle extends Canvas {
 
-    public void paint(Graphics graphics){
-        graphics.setColor(Color.red);
-        graphics.fillRect(20, 20, 100, 40);
-        graphics.setColor(Color.BLACK);
-        graphics.drawString("Red Rectangle", 20, 20);
+    public void paint(Graphics g){
+        g.setColor(Color.red);
+        g.fillRect(20, 20, 100, 40);
+        g.setColor(Color.BLACK);
+        g.drawString("Red Rectangle", 20, 20);
         setLocation(20, 15);
     }
 }
