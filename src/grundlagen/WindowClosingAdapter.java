3d67c38e2a69073bf5e4634ca35b444a991dfa57
Jitendra Kumar Yadav <jiteyad@gmail.com>
2020-07-20 01:05:57 +0200

    update

--- src/grundlagen/WindowClosingAdapter.java
+++ src/grundlagen/WindowClosingAdapter.java
@@ -1,5 +1,6 @@
 package grundlagen;
 
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,5 +10,12 @@ public class WindowClosingAdapter extends WindowAdapter {
         e.getWindow().setVisible(false);
         e.getWindow().dispose();
         System.out.println("closing window "+ e.getWindow().getName());
+    }
+
+    public static void main(String[] args) {
+        Frame window = new Frame();
+        window.addWindowListener(new WindowClosingAdapter());
+        window.setSize(400, 300);
+        window.setVisible(true);
     }
 }
